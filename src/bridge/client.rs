57d8f8a5595ab6e287bf330a490292fc3ccb492a
Ryan Leckey <ryan@launchbadge.com>
2018-11-21 10:32:42 -0800

    Add error handling to client_dial in c bridge

--- src/bridge/client.rs
+++ src/bridge/client.rs
@@ -4,16 +4,18 @@ use std::ffi::CStr;
 
 #[doc(hidden)]
 #[no_mangle]
-pub unsafe extern "C" fn hedera_client_dial(address: *const c_char) -> *mut Client {
+pub unsafe extern "C" fn hedera_client_dial(address: *const c_char, out: *mut *mut Client) -> u64 {
     debug_assert!(!address.is_null());
 
     let address = CStr::from_ptr(address);
     let address = address.to_string_lossy();
 
     let client = Client::new(address);
-    let client = Box::new(client);
+    let client = try_ffi!(client);
 
-    Box::into_raw(client)
+    *out = Box::into_raw(Box::new(client));
+
+    0
 }
 
 #[doc(hidden)]
