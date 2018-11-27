3e1ce0c4c61140dee39a8b505d04e3be40042b55
Josh <josh@launchbadge.com>
2018-11-27 13:07:38 -0800

    transaction_id from string in bridge

--- src/bridge/transaction_id.rs
+++ src/bridge/transaction_id.rs
@@ -1,6 +1,7 @@
 use crate::{AccountId, TransactionId};
 use libc::c_char;
 use mbox::MString;
+use std::ffi::CStr;
 
 #[doc(hidden)]
 #[no_mangle]
@@ -16,4 +17,18 @@ pub unsafe extern "C" fn hedera_transaction_id_to_str(p: *mut TransactionId) -> 
     MString::from_str(&(*p).to_string())
         .into_mbox_with_sentinel()
         .into_raw() as *mut c_char
+}
+
+#[doc(hidden)]
+#[no_mangle]
+pub unsafe extern "C" fn hedera_transaction_id_from_str(s: *const c_char, out: *mut TransactionId) -> u64 {
+    debug_assert!(!s.is_null());
+    debug_assert!(!out.is_null());
+
+    let s = CStr::from_ptr(s);
+    let s = s.to_string_lossy();
+
+    *out = try_ffi!(s.parse());
+
+    0
 }
