9e022687b310b9db526b684b6a175e4e26ada574
Ryan Leckey <ryan@launchbadge.com>
2018-11-28 13:49:55 -0800

    Expand recent send/receive record threshold in the builder

--- src/transaction_create_account.rs
+++ src/transaction_create_account.rs
@@ -9,6 +9,8 @@ use std::any::Any;
 pub struct TransactionCreateAccount {
     key: Option<PublicKey>,
     initial_balance: u64,
+    send_record_threshold: u64,
+    receive_record_threshold: u64,
 }
 
 interfaces!(
@@ -23,6 +25,8 @@ impl Transaction<TransactionCreateAccount> {
             TransactionCreateAccount {
                 key: None,
                 initial_balance: 0,
+                send_record_threshold: u64::max_value(),
+                receive_record_threshold: u64::max_value(),
             },
         )
     }
@@ -38,14 +42,28 @@ impl Transaction<TransactionCreateAccount> {
         self.inner().initial_balance = balance;
         self
     }
+
+    /// Create an account record for any transaction withdrawing more than this many tinybars.
+    #[inline]
+    pub fn send_record_threshold(&mut self, threshold: u64) -> &mut Self {
+        self.inner().send_record_threshold = threshold;
+        self
+    }
+
+    /// Create an account record for any transaction depositing more than this many tinybars.
+    #[inline]
+    pub fn receive_record_threshold(&mut self, threshold: u64) -> &mut Self {
+        self.inner().receive_record_threshold = threshold;
+        self
+    }
 }
 
 impl ToProto<TransactionBody_oneof_data> for TransactionCreateAccount {
     fn to_proto(&self) -> Result<TransactionBody_oneof_data, Error> {
         let mut data = proto::CryptoCreate::CryptoCreateTransactionBody::new();
         data.set_initialBalance(self.initial_balance);
-        data.set_sendRecordThreshold(i64::max_value());
-        data.set_receiveRecordThreshold(i64::max_value());
+        data.set_sendRecordThreshold(self.send_record_threshold);
+        data.set_receiveRecordThreshold(self.receive_record_threshold);
 
         let key = match self.key.as_ref() {
             Some(key) => key,
