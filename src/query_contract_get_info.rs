243b6a6dba46d72f2a2f09bd16cf44730f64062f
Jason Poindexter <jason@launchbadge.com>
2018-12-03 16:18:11 -0800

    Add Contract Get Info

---
+++ src/query_contract_get_info.rs
@@ -0,0 +1,53 @@
+use crate::{
+    proto::{self, Query::Query_oneof_query, QueryHeader::QueryHeader, ToProto},
+    query::{Query, QueryInner},
+    AccountId, Client, ErrorKind, crypto::PublicKey, ContractId, PreCheckCode,
+    timestamp::Timestamp
+};
+
+use failure::Error;
+use std::{time::Duration};
+use chrono::{DateTime, Utc};
+
+pub struct QueryContractGetInfoResponse {
+    contract_id: ContractId,
+    account_id: AccountId,
+    contract_account_id: String,
+    admin_key: Option<PublicKey>,
+    expiration_time: Option<DateTime<Utc>>,
+    auto_renew_period: Option<Duration>,
+    storage: u64,
+}
+
+pub struct QueryContractGetInfo {
+    contract: ContractId,
+}
+
+impl QueryContractGetInfo {
+    pub fn new(client: &Client, contract: ContractId) -> Query<QueryContractGetInfoResponse> {
+        Query::new(client, Self { contract })
+    }
+}
+
+impl QueryInner for QueryContractGetInfo {
+    type Response = QueryContractGetInfoResponse;
+
+    fn get(&self, mut response: proto::Response::Response) -> Result<Self::Response, Error>{
+        let mut response = response.take_contractgetInfo();
+        let header = response.take_header();
+
+        match hearer.getnodeTransactionPrecheckCode().into() {
+            PreCheckCode::Ok => Ok(response.get_balance()),
+            code => Err(ErrorKind::Precheck(code))?
+        }
+    }
+
+    fn to_query_proto(&self, header: QueryHeader) -> Result<Query_oneof_query, Error> {
+        let mut query = proto::ContractGetInfo::ContractGetInfoQuery::new();
+        query.set_header(header);
+        query.set_contractID(self.contract.to_proto()?);
+
+        Ok(Query_oneof_query::contractGetInfo(query))
+
+    }
+}
