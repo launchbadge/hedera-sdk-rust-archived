383746c8a7aacab3d4fd62d293564877319538a9
Ryan Leckey <ryan@launchbadge.com>
2018-12-05 09:13:26 -0800

    Uncomment contract_get_info mod

--- src/lib.rs
+++ src/lib.rs
@@ -22,7 +22,7 @@ mod query_file_get_contents;
 mod query_file_get_info;
 mod query_get_transaction_receipt;
 mod query_transaction_get_record;
-// todo: mod query_contract_get_info;
+mod query_contract_get_info;
 mod timestamp;
 pub mod transaction;
 
