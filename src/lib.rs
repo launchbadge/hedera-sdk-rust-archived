42c8ccdcaeeb6b0989804ce1cfbf1be69666b705
Josh <josh@launchbadge.com>
2018-12-04 16:55:05 -0800

    adds query crypto get claim

--- src/lib.rs
+++ src/lib.rs
@@ -29,6 +29,7 @@ pub mod query;
 mod query_contract_get_bytecode;
 mod query_contract_get_info;
 mod query_crypto_get_account_balance;
+mod query_crypto_get_claim;
 mod query_crypto_get_account_records;
 mod query_crypto_get_info;
 mod query_file_get_contents;
