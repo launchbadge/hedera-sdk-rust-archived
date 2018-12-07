087a0b760ea09a9c7f901890f4f40f85cbd71f8e
Jason Poindexter <jason@launchbadge.com>
2018-12-06 18:10:10 -0800

    Rework add python verison of CryptoGetInfo

--- src/bridge/python/mod.rs
+++ src/bridge/python/mod.rs
@@ -5,6 +5,7 @@ mod query_contract_get_bytecode;
 mod query_crypto_get_account_balance;
 mod query_file_get_contents;
 mod query_get_transaction_receipt;
+mod query_crypto_get_info;
 mod transaction_receipt;
 
 use self::client::PyClient;
