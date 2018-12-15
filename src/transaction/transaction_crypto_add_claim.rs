5ecdd04924f9e694402a7b1aea2ad495e35de8a4
Josh <josh@launchbadge.com>
2018-12-14 17:07:31 -0800

    made add claim build the claim instead of accept the claim

--- src/transaction/transaction_crypto_add_claim.rs
+++ src/transaction/transaction_crypto_add_claim.rs
@@ -1,9 +1,11 @@
 use crate::{
+    crypto::PublicKey,
     claim::Claim,
     id::AccountId,
     proto::{self, ToProto, Transaction::TransactionBody_oneof_data},
     transaction::Transaction,
     Client,
+    ErrorKind
 };
 use failure::Error;
 use query_interface::{interfaces, vtable_for};
@@ -12,7 +14,8 @@ use std::any::Any;
 #[derive(Debug)]
 pub struct TransactionCryptoAddClaim {
     account: AccountId,
-    claim: Option<Claim>,
+    hash: Option<Vec<u8>>,
+    keys: Vec<PublicKey>,
 }
 
 interfaces!(
@@ -26,7 +29,8 @@ impl TransactionCryptoAddClaim {
             client,
             Self {
                 account,
-                claim: None,
+                hash: None,
+                keys: Vec::new(),
             },
         )
     }
@@ -34,8 +38,14 @@ impl TransactionCryptoAddClaim {
 
 impl Transaction<TransactionCryptoAddClaim> {
     #[inline]
-    pub fn claim(&mut self, claim: Claim) -> &mut Self {
-        self.inner().claim = Some(claim);
+    pub fn hash(&mut self, hash: Vec<u8>) -> &mut Self {
+        self.inner().hash = Some(hash);
+        self
+    }
+
+    #[inline]
+    pub fn add_key(&mut self, key: PublicKey) -> &mut Self {
+        self.inner().keys.push(key);
         self
     }
 }
@@ -45,9 +55,18 @@ impl ToProto<TransactionBody_oneof_data> for TransactionCryptoAddClaim {
         let mut data = proto::CryptoAddClaim::CryptoAddClaimTransactionBody::new();
         data.set_accountID(self.account.to_proto()?);
 
-        if let Some(claim) = &self.claim {
-            data.set_claim(claim.to_proto()?);
-        }
+        let hash = match self.hash.clone() {
+            Some(hash) => hash,
+            None => Err(ErrorKind::MissingField("hash"))?
+        };
+
+        let claim = Claim{
+            account: self.account,
+            hash,
+            keys: self.keys.clone()
+        };
+
+        data.set_claim(claim.to_proto()?);
 
         Ok(TransactionBody_oneof_data::cryptoAddClaim(data))
     }
