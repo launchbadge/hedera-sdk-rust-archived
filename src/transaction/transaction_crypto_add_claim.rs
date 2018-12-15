12efa64b6feae3c4bfd197f4758c9f85bebc3c3d
Josh <josh@launchbadge.com>
2018-12-14 16:25:26 -0800

    made add claim take the claim from a builder function instead of as part of the constructor

--- src/transaction/transaction_crypto_add_claim.rs
+++ src/transaction/transaction_crypto_add_claim.rs
@@ -12,7 +12,7 @@ use std::any::Any;
 #[derive(Debug)]
 pub struct TransactionCryptoAddClaim {
     account: AccountId,
-    claim: Claim,
+    claim: Option<Claim>,
 }
 
 interfaces!(
@@ -21,8 +21,22 @@ interfaces!(
 );
 
 impl TransactionCryptoAddClaim {
-    pub fn new(client: &Client, account: AccountId, claim: Claim) -> Transaction<Self> {
-        Transaction::new(client, Self { account, claim })
+    pub fn new(client: &Client, account: AccountId) -> Transaction<Self> {
+        Transaction::new(
+            client,
+            Self {
+                account,
+                claim: None,
+            },
+        )
+    }
+}
+
+impl Transaction<TransactionCryptoAddClaim> {
+    #[inline]
+    pub fn claim(&mut self, claim: Claim) -> &mut Self {
+        self.inner().claim = Some(claim);
+        self
     }
 }
 
@@ -30,7 +44,10 @@ impl ToProto<TransactionBody_oneof_data> for TransactionCryptoAddClaim {
     fn to_proto(&self) -> Result<TransactionBody_oneof_data, Error> {
         let mut data = proto::CryptoAddClaim::CryptoAddClaimTransactionBody::new();
         data.set_accountID(self.account.to_proto()?);
-        data.set_claim(self.claim.to_proto()?);
+
+        if let Some(claim) = &self.claim {
+            data.set_claim(claim.to_proto()?);
+        }
 
         Ok(TransactionBody_oneof_data::cryptoAddClaim(data))
     }
